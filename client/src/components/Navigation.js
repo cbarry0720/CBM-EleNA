a163623d0102459c50e107e298546b8523d72e5a
cbarry0720 <chrisb0720@gmail.com>
2022-11-03 20:12:50 -0400

    navigation component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "../styles/navigation.css";
 
 export default function Navigation() {
-	return <div></div>;
+	return (
+		<div className="nav-container">
+			<input className="from" placeholder="From" />
+			<input className="to" placeholder="To" />
+			<select className="select">
+				<option>Maximize Elevation</option>
+				<option>Minimize Elevation</option>
+			</select>
+		</div>
+	);
 }
