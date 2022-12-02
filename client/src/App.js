667fe39845f15ab0722c05ec4802a920658d2ed2
cbarry0720 <chrisb0720@gmail.com>
2022-12-02 17:38:38 -0500

    Searching To and From Locations

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import Map from "./components/Map";
 import Navigation from "./components/Navigation";
 
 function App() {
+	const [pointA, setPointA] = useState([]);
+	const [pointB, setPointB] = useState([]);
+
 	return (
 		<div>
-			<Navigation />
-			<Map />
+			<Navigation setPointA={setPointA} setPointB={setPointB} />
+			<Map pointA={pointA} pointB={pointB} />
 		</div>
 	);
 }
