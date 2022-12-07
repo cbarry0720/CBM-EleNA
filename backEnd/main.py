7490e8046503c91234a5163b11fa905e1bb9f0c9
Bryan McCaffery <bryanmccaf@gmail.com>
2022-12-07 17:36:28 -0500

    Update Algorithm

--- backEnd/main.py
+++ backEnd/main.py
@@ -49,17 +49,16 @@ for x in lists:
         xValues[x['longitude']] = {x['latitude']:x}
     else:
         xValues[x['longitude']][x['latitude']] = x
-print(len(xValues))
-for z in xValues:
-    if len(xValues[z]) == 1:
-        print(xValues[z])
-        break
+
+
 for key in G.nodes.keys():
     node = G.nodes.get(key)
     xval = node['x']
     yval = node['y']
     node['elevation'] = xValues[xval][yval]['elevation']
-    print(node)
+    if node['elevation'] is None:
+        print("Error")
+
 
 
 #for x in lists:
@@ -72,25 +71,63 @@ for key in G.nodes.keys():
 #f.close()
 
 #print(requests.get(url[:-1]).text)
-def getPathBetween(G,location1, location2,multiplier):
-    nodeToMapToTup = ox.geocode(location1)
-    nodeToMapFromTup = ox.geocode(location2)
-    node1 = ox.nearest_nodes(G,nodeToMapFromTup[1],nodeToMapFromTup[0])
-    node2 = ox.nearest_nodes(G,nodeToMapToTup[1],nodeToMapToTup[0])
-    return nx.shortest_path(G,node1,node2)
+def getPathBetween(G,start, end,k=2):
+    # get the gecode location for the start and end location
+    start_loc = ox.geocode(start)
+    end_loc = ox.geocode(end)
+
+    # get the nearest node in the grapgh of amherst to the ask locations
+    start_node = ox.nearest_nodes(G, start_loc[1], start_loc[0])
+    end_node = ox.nearest_nodes(G, end_loc[1], end_loc[0])
+
+    # get the k shortest paths from start location to end location
+    paths = ox.k_shortest_paths(G, start_node, end_node, k)
+
+    K_paths = list(paths)
+    best_path = []
+    best_val = 99999999999
+    best_elevation = 0
+    best_dist = 0
+    # for every path returned calculate the distance and total elevation change then return shortest path with smallest changes
+    for path in K_paths:
+        dist = 0
+        elevation_change = 0
+
+        for i in range(0, len(path) - 1):
+            node1 = path[i]
+            node2 = path[i + 1]
+            # Get lat and long of nodes
+            y1 = G.nodes[node1]['y']
+            x1 = G.nodes[node1]['x']
+
+            y2 = G.nodes[node2]['y']
+            x2 = G.nodes[node2]['x']
+
+            dist += ox.distance.euclidean_dist_vec(y1, x1, y2, x2)
+
+            a_elevation = G.nodes[node1]['elevation']
+            b_elevation = G.nodes[node2]['elevation']
+
+            elevation_change += abs(a_elevation - b_elevation)
+
+        if dist + elevation_change < best_val:
+            best_val = dist + elevation_change
+            best_elevation = elevation_change
+            best_dist = dist
+            best_path = path
+    return best_path,best_elevation,best_dist
 
 app = Flask(__name__)
 @app.route('/route')
 def query_route():
     start = request.args.get('start')
     finish = request.args.get('finish')
-    multiplier = request.args.get('routeMultiplier')
+    multiplier = int(request.args.get('routeMultiplier'))
     pathBetween = getPathBetween(G,start,finish,multiplier)
     output = list()
-    for x in pathBetween:
+    for x in pathBetween[0]:
         output.append(G.nodes.get(x))
-    result = outputObject(output,0,0)
-
+    result = outputObject(output,pathBetween[2],pathBetween[1])
     return result.toJSON()
 class outputObject:
     def __init__(self,route,totalDistance,totalElevation):
